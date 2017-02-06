b20bb5f0825bbc51a5ca8968867c97f09d4887c3
mumu <zengmumu@126.com>
2017-02-06 17:18:05 +0800

    弹出层

--- src/static/js/que.js
+++ src/static/js/que.js
@@ -65,7 +65,64 @@ $(function(){
 		$("footer").css("bottom",-160);
 	}
 	
+	$(".s4 li").eq(0).click(function(){
+		var data1=["轮播1","轮播2","轮播3"];
+		$(".modal").slider({data:data1})
+		$.fn.fullpage.setAllowScrolling(false);//不让插件滚动
+	})
+	$(".s4 li").eq(1).click(function(){
+		var data1=["hello1","hello2","hello3","hello4"];
+		$(".modal").slider({data:data1})
+		$.fn.fullpage.setAllowScrolling(false);//不让插件滚动
+	})
+	
 })
+;(
+	function($){
+		$.fn.extend({
+			"slider":function(opt){
+				this.each(function(){
+					var dom = $(this);
+					// 指定数据渲染html节点
+					var str ="";
+					for(let i=0;i<opt.data.length;i++){
+						str+='<div class="slide">'+opt.data[i]+'</div>'
+					}
+					dom.find(".slider_content").html(str).css("width",1150*opt.data.length)
+					// 默认打开显示
+					dom.show();
+					// 1，什么时候滑动
+  					// 2，怎么实现滑动
+   					// 3，滑动多少
+   					// 4，滑动到第几个了 （默认滑动到第1个，没单击滑动到个数增加）	
+					var index = 1; //滑动到第几个了
+					dom.find(".slide_num").html(index+"/"+opt.data.length);
+					dom.find(".next").click(function(){
+					if(index>=opt.data.length){return}
+					index++;
+					dom.find(".slider_content").css("transform","translateX("+(index-1)*-1150+"px)");
+					dom.find(".slide_num").html(index+"/"+opt.data.length);
+					
+					})
+					dom.find(".pre").click(function(){
+					if(index<=1){return}
+					index--;
+					dom.find(".slider_content").css("transform","translateX("+(index-1)*-1150+"px)");
+					dom.find(".slide_num").html(index+"/"+opt.data.length);
+					
+					})
+					// 单击close 关闭自己
+					dom.find(".close").click(function(){
+						dom.hide();
+						$.fn.fullpage.setAllowScrolling(true);
+					})
+					
+				})
+				
+			}
+		})
+	}
+)(jQuery)
 
 ;(
 	function($){
